6a7c7cdd02f4fb99497186a758f55961de4c4bdb
dennis belio <dbelio39@gmail.com>
2018-08-18 00:05:15 +0300

    stackoverflow_lite directories

---
+++ app/views.py
@@ -0,0 +1,52 @@
+from flask_api import FlaskAPI
+from flask import jsonify, request
+
+from app.models import Questions, questions, Answers, answers
+
+def create_app(config_name):
+    
+    app = FlaskAPI(__name__, instance_relative_config=True)
+
+# Questions
+
+    @app.route('/api/v1/questions', methods=['GET'])
+    def get_all_questions():
+        return jsonify({'questions': questions})
+
+    @app.route('/api/v1/questions/<int:id>', methods=['GET'])
+    def get_one_question(id):
+        quest = [question for question in questions if question['id'] == id ] or None
+        if quest:
+            return jsonify({'question': quest[0]})
+        else:
+            return jsonify({'message': "specific question not found"})
+        return jsonify({'message': "You have no question yet"})    
+
+    @app.route('/api/v1/questions', methods=['POST'])
+    def create_questions():
+        data = request.get_json()
+        question = data.get('question')
+        date_posted = data.get('date_posted')
+        new_question = Questions(question, date_posted)
+        return jsonify({
+            'message': 'success, Question created',
+            'question': new_question.save()
+        }), 201
+
+    @app.route('/api/v1/questions/<int:id>/answers', methods=['POST'])
+    def post_answer(id):
+        question = [question for question in questions if question['id'] == id ]
+        if len(question) == 0:
+            return jsonify({'message': 'No question found'})
+        
+        data = request.get_json()
+        answer = data.get('answer')
+        date_posted = data.get('date_posted')
+        post_answer = Answers(answer, date_posted)
+        return jsonify({'Question': question}, {
+                    'message': 'success, Answer created',
+                    'Answer': post_answer.save()
+                }), 201
+
+    return app
+
---
+++ tests/test_questions.py
@@ -0,0 +1,51 @@
+import unittest
+import os
+import json
+
+from app.views import create_app
+from app.models import Questions, Answers
+
+class StackOverflow_lite(unittest.TestCase):
+    """This class represents Questions and Answers posted."""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client()
+        self.questions = {'Question': 'What is a flask restful api', 'Date posted': '12th August 2018'}
+        self.answers = {'Question': 'Flask restful api', 'Date posted': '14th August 2018'}
+        
+    def test_post_question(self):
+        """Testing posting a question."""
+        response = self.client.post(
+            '/api/v1/questions', data=json.dumps(self.questions), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+    def test_view_all_questions(self):
+        """Test to view all questions."""
+        response = self.client.get(
+            '/api/v1/questions', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_one_question(self):
+        """Test to get single question."""
+        response = self.client.get(
+            '/api/v1/questions/1', data=json.dumps(self.questions), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_answer_to_question(self):
+        """Test to post answer"""
+        response = self.client.post(
+            '/api/v1/questions/1/answers', data=json.dumps(self.answers), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_no_question(self):
+        """Test to no question"""
+        response = self.client.get( 
+            '/api/v1/questions/88', data=json.dumps(self.answers), content_type='application/json')  
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.get_data())
+        self.assertEqual(data['message'], "specific question not found")
+
+if __name__ == "__main__":
+    unittest.main()
